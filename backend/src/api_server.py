84d96b4126b23f79ef01765048f1b7af152baf07
mhnu16 <manoritamar@gmail.com>
2024-02-22 15:09:07 +0200

    Removed unnecessary methods in the API Server

--- backend/src/api_server.py
+++ backend/src/api_server.py
@@ -13,22 +13,12 @@ class ApiServer:
 
     def __init__(self, database: Database):
         self.database = database
-        self.app = flask.Flask(
-            __name__, static_folder=SERVER.SERVE_PATH, static_url_path=""
-        )
+        self.app = flask.Flask(__name__)
         self.cors = flask_cors.CORS(self.app, resources={r"/api/*": {"origins": "*"}})
 
         @self.app.route("/api/rand")
         def rand():
             return flask.jsonify(rand=random())
-
-        @self.app.route("/")
-        def index():
-            return flask.send_from_directory(SERVER.SERVE_PATH, "index.html")
-
-        @self.app.route("/<path:path>")
-        def static_files(path):
-            return flask.send_from_directory(SERVER.SERVE_PATH, path)
 
     def serve(self, debug=False):
         context = ssl.SSLContext()
